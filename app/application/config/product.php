f5d6af6ad3d70037c7789174cd5e0b692e986e7f
jhilirani <jhilirani@gmail.com>
2016-09-15 01:37:32 +0530

    change server key for firbbase

--- app/application/config/product.php
+++ app/application/config/product.php
@@ -23,4 +23,4 @@ $config['expandableMemory']=array(1=>'16 GB','2'=>'32 GB','3'=>'64 GB');
 $config['expandableMemory']=array(1=>'16 GB','2'=>'32 GB','3'=>'64 GB');
 $config['warrantyDuration']=array(1=>'6 Months','2'=>'12 Months','3'=>'18 Months','4'=>'24 Months');
 
-$config['GoogleGSMKEY']='AIzaSyB_gXlvSsAG8giDMJbkGZy-h-cRRZQWO_g';
+$config['GoogleGSMKEY']='AIzaSyBBgQJMNqsOczq6lhjzK-ouYx0daP7Vbt8';
